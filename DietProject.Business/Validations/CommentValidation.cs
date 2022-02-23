0305d2a776989ff981a5b177f62949538eae675a
Kürşat Turgut <kursat.turgut@ntm.com.tr>
2022-02-23 22:32:28 +0300

    project delelete

---
+++ DietProject.Business/Validations/CommentValidation.cs
@@ -0,0 +1,27 @@
+﻿using DietProject.Core.ExtensionEF;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietProject.Business.Validations
+{
+    //https://docs.fluentvalidation.net/en/latest/
+    public class CommentValidation : AbstractValidator<Comment>
+    {
+        public CommentValidation()
+        {
+            RuleFor(x => x.Score)
+                .NotEmpty().WithMessage("Skor belirlemeniz gerekmektedir.")
+                .GreaterThanOrEqualTo(0).WithMessage("Skorunuz 0'dan büyük olmalıdır.");
+
+            RuleFor(x => x.CommentText)
+                .NotEmpty().WithMessage("Yorum girmek zorunludur.");
+
+            RuleFor(x => x.CommentDate)
+                .NotNull().WithMessage("Yorum tarihi boş olamaz.");
+        }
+    }
+}
--- DietProject.MVCUI/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application Codebehind="Global.asax.cs" Inherits="DietProject_MVCUI.MvcApplication" Language="C#" %>
