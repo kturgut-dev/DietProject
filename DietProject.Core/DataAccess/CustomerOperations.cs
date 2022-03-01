e261e2f3d65e1bccf40691e5f95792cb903803b4
Feyza Öztürk <170101017@ogrenci.yalova.edu.tr>
2022-03-01 20:10:43 +0300

    operations tamamlandı

---
+++ DietProject.Core/DataAccess/CustomerOperations.cs
@@ -0,0 +1,97 @@
+﻿using DietProject.Core.Entities;
+using DietProject.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietProject.Core.DataAccess
+{
+    public class CustomerOperations : IBaseOperations<Customer>
+    {
+        public bool Add(Customer entity)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    context.Customers.Add(entity);
+                    context.SaveChanges();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool Delete(Customer entity)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    context.Customers.Remove(entity);
+                    context.SaveChanges();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public Customer Get(Expression<Func<Customer, bool>> prop)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    return context.Customers.Find(prop);
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public IList<Customer> GetAll(Expression<Func<Customer, bool>> prop)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    return context.Customers.Where(prop).ToList();
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public bool Update(Customer entity)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ DietProject.Core/DataAccess/DietDetailOperations.cs
@@ -0,0 +1,97 @@
+﻿using DietProject.Core.Entities;
+using DietProject.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietProject.Core.DataAccess
+{
+    public class DietDetailOperations : IBaseOperations<DietDetail>
+    {
+        public bool Add(DietDetail entity)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    context.DietDetails.Add(entity);
+                    context.SaveChanges();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool Delete(DietDetail entity)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    context.DietDetails.Remove(entity);
+                    context.SaveChanges();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public DietDetail Get(Expression<Func<DietDetail, bool>> prop)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    return context.DietDetails.Find(prop);
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public IList<DietDetail> GetAll(Expression<Func<DietDetail, bool>> prop)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    return context.DietDetails.Where(prop).ToList();
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public bool Update(DietDetail entity)
+        {
+            using (DietProjectContext context = new DietProjectContext())
+            {
+                try
+                {
+                    context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
