6ab903ad6b8692e94dce8da05bd2b80ee91add35
Feyza Öztürk <170101017@ogrenci.yalova.edu.tr>
2022-03-07 22:34:39 +0300

    transfer

---
+++ DietProject.Web/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DietProject.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Route("Login")]
+        public IActionResult Login()
+        {
+            ViewData["Title"] = "Giriş Yap";
+            return View();
+        }
+
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            ViewData["Title"] = "Kayıt Ol";
+            return View();
+        }
+    }
+}
